420bde866068fcdb26710e95fa7bf9ae19de81ab
Jerolock <163981038+Jerolock@users.noreply.github.com>
2024-08-06 12:56:41 +0500

    Update module_13_5.py

--- module_13_5.py
+++ module_13_5.py
@@ -22,7 +22,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 # import asyncio
 import logging
 
-API = '7404531016:AAHYQUWSSvxhtCJ_fy0XJi3Qzgddk9OxQYI'
+API = 'XXX'
 bot = Bot(token=API)
 dp = Dispatcher(bot, storage=MemoryStorage())
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
